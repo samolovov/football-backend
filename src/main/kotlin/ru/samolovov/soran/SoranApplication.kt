120a7b203243fa037b42176a5beec2a96f9b93af
Pavel Samolovov <psamolovov@gmail.com>
2020-01-30 22:19:47 +0700

    Project initial structure

---
+++ src/main/kotlin/ru/samolovov/soran/SoranApplication.kt
@@ -0,0 +1,11 @@
+package ru.samolovov.soran
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoranApplication
+
+fun main(args: Array<String>) {
+	runApplication<SoranApplication>(*args)
+}
---
+++ src/test/kotlin/ru/samolovov/soran/SoranApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.samolovov.soran
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoranApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
