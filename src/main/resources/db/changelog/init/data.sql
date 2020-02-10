f3ac39f3b367ca205841b3e7656c38854ed48d6f
psamolovov <psamolovov@gmail.com>
2020-02-11 00:34:54 +0700

    Add initial tables structures and test data

---
+++ src/main/resources/db/changelog/init/data.sql
@@ -0,0 +1,17 @@
+insert into referees values (1, 'Konstantin', 'Rachenko', '1989-12-12','');
+insert into teams values (1, 'NSU Stars', '2019-06-06', 'Сборная звёзд НГУ прошлых лет', '');
+insert into teams values (2, 'Domino', '2010-01-01', 'Легендарное Домино', '');
+insert into players values (1, 'Pavel', 'Samolovov', '1988-02-24', 'MIDFIELDER', 'Родился в городе Саяногорск, Хакасия', '');
+insert into players values (2, 'Igor', 'Arefiev', '1999-01-01', 'DEFENDER', 'Очень маленький игрок', '');
+insert into tournaments values (1, 'Зимнее первенство СОРАН');
+insert into seasons values (1, 1, '2019-11-10', '2020-04-01');
+insert into season_teams values (1, 1, 1);
+insert into season_team_players values (1, 1, 1, 1);
+insert into season_team_players values (2, 1, 2, 2);
+insert into games values (1, 1, 1, 2, 3, 0, 1);
+-- game details
+insert into game_details values (1, 1, 1, 1, 'goal', 20);
+insert into game_details values (2, 1, 1, 1, 'goal', 25);
+insert into game_details values (3, 1, 1, 1, 'goal', 50);
+insert into game_details values (4, 1, 2, 2, 'red', 45);
+
