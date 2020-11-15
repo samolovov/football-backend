d751faa9ec79f9c66c48b6900fbca9e15b9c6fc5
Pavel.Samolovov <Pavel.Samolovov@nexign-systems.com>
2020-11-15 19:51:14 +0700

    Add validation for game

--- src/main/kotlin/ru/samolovov/soran/dto/GameDetailsDto.kt
+++ src/main/kotlin/ru/samolovov/soran/dto/GameDetailsDto.kt
@@ -1,11 +1,14 @@
 package ru.samolovov.soran.dto
 
+import javax.validation.constraints.PositiveOrZero
+
 class GameDetailsDto(
     val id: Long? = null,
     val gameId: Long,
     val teamId: Long,
     val playerId: Long,
     val type: Type,
+    @field:PositiveOrZero
     val minute: Int
 )
 
--- src/main/kotlin/ru/samolovov/soran/dto/GameDto.kt
+++ src/main/kotlin/ru/samolovov/soran/dto/GameDto.kt
@@ -1,14 +1,19 @@
 package ru.samolovov.soran.dto
 
 import java.time.LocalDate
+import javax.validation.constraints.PastOrPresent
+import javax.validation.constraints.PositiveOrZero
 
 class GameDto(
     val id: Long? = null,
     val seasonId: Long,
     val firstTeamId: Long,
     val secondTeamId: Long,
+    @field:PositiveOrZero
     val firstTeamGoals: Int,
+    @field:PositiveOrZero
     val secondTeamGoals: Int,
+    @field:PastOrPresent
     val date: LocalDate,
     val refereeId: Long
 )
