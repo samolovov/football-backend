02c741474d4f1b214c32e783ea7c9fbb92cfac12
Pavel.Samolovov <Pavel.Samolovov@nexign-systems.com>
2020-11-15 19:48:13 +0700

    Add validation for referee

--- src/main/kotlin/ru/samolovov/soran/dto/RefereeDto.kt
+++ src/main/kotlin/ru/samolovov/soran/dto/RefereeDto.kt
@@ -1,11 +1,16 @@
 package ru.samolovov.soran.dto
 
 import java.time.LocalDate
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Past
 
 data class RefereeDto(
     val id: Long? = null,
+    @field:NotBlank
     val firstName: String,
+    @field:NotBlank
     val lastName: String,
     val avatar: String? = null,
+    @field:Past
     val birthday: LocalDate? = null
 )
