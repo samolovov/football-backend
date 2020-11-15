9a75aef9f3f1a6480ff61537c0b3295e1943fe68
Pavel.Samolovov <Pavel.Samolovov@nexign-systems.com>
2020-11-15 19:23:30 +0700

    Add validation for player

--- src/main/kotlin/ru/samolovov/soran/dto/PlayerDto.kt
+++ src/main/kotlin/ru/samolovov/soran/dto/PlayerDto.kt
@@ -1,13 +1,18 @@
 package ru.samolovov.soran.dto
 
 import java.time.LocalDate
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Past
 
 data class PlayerDto(
     val id: Long? = null,
+    @field:NotBlank
     val firstName: String,
+    @field:NotBlank
     val lastName: String,
     val avatar: String? = null,
     val position: Position? = null,
+    @field:Past
     val birthday: LocalDate? = null
 )
 
